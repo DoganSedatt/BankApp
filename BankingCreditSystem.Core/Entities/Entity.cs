23c9051b49a7dc2e987886eaec24200da0d2dced
Sedat Doğan <dogannnsedat@gmail.com>
2025-02-19 18:27:28 +0300

    Core katmanına generic ve base bir entity oluşturdum

---
+++ BankingCreditSystem.Core/Entities/Entity.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BankingCreditSystem.Core.Entities;
+
+public abstract class Entity<TId>
+{
+    public TId Id { get; set; }=default!;
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+    
+
+   protected Entity()
+    {
+        CreatedDate = DateTime.UtcNow;
+    }
+} 
